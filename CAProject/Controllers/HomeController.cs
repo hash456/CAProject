a5a2ba49147668982e3d1b398c36c659e843bca5
TingKai911 <tingkai911@gmail.com>
2020-10-09 07:55:42 +0800

    added tempCart in sessionStorage

--- CAProject/Controllers/HomeController.cs
+++ CAProject/Controllers/HomeController.cs
@@ -32,7 +32,19 @@ namespace CAProject.Controllers
                 ViewData["DisplayLogout"] = "YES";
             }
 
-            return View(await PaginatedList<Product>.CreateAsync(db.Product,pageNumber ?? 1, pageSize));
+            PaginatedList<Product> paginatedList = await PaginatedList<Product>.CreateAsync(db.Product, pageNumber ?? 1, pageSize);
+            ViewData["paginatedList"] = paginatedList;
+
+            Dictionary<int, int> paginatedStockCount = new Dictionary<int, int>();
+            foreach(Product p in paginatedList)
+            {
+                paginatedStockCount.Add(p.Id, Convert.ToInt32(db.ActivationCode.Where(
+                                                    x => x.ProductId == p.Id && x.IsSold == true).Count()));
+            }
+
+            ViewData["paginatedStockCount"] = paginatedStockCount;
+
+            return View();
         }
 
         public IActionResult Privacy()
